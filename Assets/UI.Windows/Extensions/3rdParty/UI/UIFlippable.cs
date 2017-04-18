8210c6daa76d4b82bf6e5eafcee8a18d9c7737ed
Alex <chrome.alex@gmail.com>
2017-04-18 11:54:54 +0300

    flippable fix

--- Assets/UI.Windows/Extensions/3rdParty/UI/UIFlippable.cs
+++ Assets/UI.Windows/Extensions/3rdParty/UI/UIFlippable.cs
@@ -4,14 +4,13 @@ using System.Collections.Generic;
 
 namespace UnityEngine.UI {
 
-	[/*RequireComponent(typeof(RectTransform)), RequireComponent(typeof(Graphic)),*//* DisallowMultipleComponent, */AddComponentMenu("UI/Flippable")]
+	[/*RequireComponent(typeof(RectTransform), typeof(Graphic)), */DisallowMultipleComponent]
+	[AddComponentMenu("UI/Effects/Extensions/Flippable")]
 	public class UIFlippable : MonoBehaviour, IMeshModifier {
 		
-		[SerializeField]
-		private bool m_Horizontal = false;
-		[SerializeField]
-		private bool m_Veritical = false;
-		
+		[SerializeField] private bool m_Horizontal = false;
+		[SerializeField] private bool m_Veritical = false;
+
 		/// <summary>
 		/// Gets or sets a value indicating whether this <see cref="UnityEngine.UI.UIFlippable"/> should be flipped horizontally.
 		/// </summary>
@@ -20,7 +19,7 @@ namespace UnityEngine.UI {
 			get { return this.m_Horizontal; }
 			set { this.m_Horizontal = value; }
 		}
-		
+
 		/// <summary>
 		/// Gets or sets a value indicating whether this <see cref="UnityEngine.UI.UIFlippable"/> should be flipped vertically.
 		/// </summary>
@@ -29,47 +28,32 @@ namespace UnityEngine.UI {
 			get { return this.m_Veritical; }
 			set { this.m_Veritical = value; }
 		}
-		
+
 		protected void OnValidate() {
-
-			if (Application.isPlaying == true) return;
-			#if UNITY_EDITOR
-			if (UnityEditor.EditorApplication.isUpdating == true) return;
-			#endif
-
 			this.GetComponent<Graphic>().SetVerticesDirty();
 		}
 
-		public void ModifyMesh(VertexHelper helper) {
+		public void ModifyMesh(VertexHelper verts) {
+			RectTransform rt = this.transform as RectTransform;
 
-			var list = new List<UIVertex>();
-			helper.GetUIVertexStream(list);
+			for (int i = 0; i < verts.currentVertCount; ++i) {
+				UIVertex uiVertex = new UIVertex();
+				verts.PopulateUIVertex(ref uiVertex, i);
 
-			this.ModifyVertices(list);  // calls the old ModifyVertices which was used on pre 5.2
+				// Modify positions
+				uiVertex.position = new Vector3(
+					(this.m_Horizontal ? (uiVertex.position.x + (rt.rect.center.x - uiVertex.position.x) * 2) : uiVertex.position.x),
+					(this.m_Veritical ? (uiVertex.position.y + (rt.rect.center.y - uiVertex.position.y) * 2) : uiVertex.position.y),
+					uiVertex.position.z
+				);
 
-			helper.AddUIVertexTriangleStream(list);
-
+				// Apply
+				verts.SetUIVertex(uiVertex, i);
+			}
 		}
 
-		public void ModifyMesh (Mesh mesh) {}
-
-		public void ModifyVertices(List<UIVertex> verts) {
-			RectTransform rt = this.transform as RectTransform;
-			
-			for (int i = 0; i < verts.Count; ++i) {
-				UIVertex v = verts[i];
-				
-				// Modify positions
-				v.position = new Vector3(
-					(this.m_Horizontal ? (v.position.x + (rt.rect.center.x - v.position.x) * 2f) : v.position.x),
-					(this.m_Veritical ? (v.position.y + (rt.rect.center.y - v.position.y) * 2f) : v.position.y),
-					v.position.z
-				);
-				
-				// Apply
-				verts[i] = v;
-
-			}
+		public void ModifyMesh(Mesh mesh) {
+			//Obsolete member implementation
 		}
 	}
 }
